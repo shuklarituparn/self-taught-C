8743c92d18576d6f33ddefef99d9048520bbbef4
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-14 20:56:39 +0300

    trying to find matching element in an array

--- Competitive coding etc/codewars/Question4/solution.c
+++ Competitive coding etc/codewars/Question4/solution.c
@@ -2,3 +2,31 @@
 // Created by rituparn on 14.02.23.
 //
 
+#include <stdio.h>
+
+
+int checkingIsogram(char *array);
+
+int main(){
+
+    char string [50];
+    printf("Enter a string: ");
+    scanf("%s",string);
+
+    printf("The string is isogram or not: %d", checkingIsogram(string));
+
+}
+
+int checkingIsogram(char *array){
+
+
+    for (size_t i = 0; i< sizeof(array); ++i) {
+        int count =0;
+        for (size_t j = 1; j< sizeof(array); ++j) {
+            if(array[i]==array[j]){
+                count=1;
+            }
+        }
+    }
+    return
+}
--- char_in_a_string.c
+++ char_in_a_string.c
@@ -1,3 +1,38 @@
 //
 // Created by rituparn on 14.02.23.
 //
+#include <stdio.h>
+
+
+int numberOfcharacter(char *array);
+
+int main(){
+
+    char string [50];
+    printf("Enter a string: ");
+    scanf("%s",string);
+
+    printf("The number of characters in the string is: ");
+    printf("%d", numberOfcharacter(string));
+
+}
+
+int numberOfcharacter(char *array){
+
+    int count=0;
+    //const size_t x= sizeof(array);
+    for(int i=0; array[i]!= '\0'; ++i){
+        for (int j = 1; array[j]!= '\0'; ++j) {
+            if(array[i]== array[j]){
+                printf("Matching element found: ");
+            }
+            else{
+                printf("Matching element not found");
+            }
+
+        }
+        //printf("%c", array[i]);
+        count++;
+    }
+    return count;
+}
