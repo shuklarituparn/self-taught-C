8c4b533db7c3ad5c052e5783eb7bf4b2c124c1c6
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-14 23:31:42 +0300

    Finally fixed the errror

--- Competitive coding etc/codewars/Question4/solution.c
+++ Competitive coding etc/codewars/Question4/solution.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
 
 int checkingIsogram(char *array);
@@ -12,112 +13,24 @@ int main(){
     char string [50];
     printf("Enter a string: ");
     scanf("%s",string);
+    //checkingIsogram(string);
 
-    printf("The string is isogram or not: %d", checkingIsogram(string));
+   printf("The string is isogram or not: %d", checkingIsogram(string));
 
 }
 
 int checkingIsogram(char *array){
 
-    int count =0;
     for (size_t i = 0; array[i] !='\0'; ++i) {
-        printf("%c---i\n", array[i]);
+        //int count =0;
+        //printf("%c---i\n", array[i]);
         for (size_t j = 1; array[j]  != '\0'; ++j) {
-            printf("%c---j\n", array[j]);
+            if(array[i]==array[j]){
+                return false;
+            }
+            else
+                return true;
         }
     }
-    return count;
+    //printf("count %d", count1);
 }
-
-/*
-Weird output
-
-Enter a string: abcde
-a---i
-b---j
-a---i
-c---j
-a---i
-d---j
-a---i
-e---j
-a---i
----j
-a---i
----j
-b---i
-b---j
-b---i
-c---j
-b---i
-d---j
-b---i
-e---j
-b---i
----j
-b---i
----j
-c---i
-b---j
-c---i
-c---j
-c---i
-d---j
-c---i
-e---j
-c---i
----j
-c---i
----j
-d---i
-b---j
-d---i
-c---j
-d---i
-d---j
-d---i
-e---j
-d---i
----j
-d---i
----j
-e---i
-b---j
-e---i
-c---j
-e---i
-d---j
-e---i
-e---j
-e---i
----j
-e---i
----j
----i
-b---j
----i
-c---j
----i
-d---j
----i
-e---j
----i
----j
----i
----j
----i
-b---j
----i
-c---j
----i
-d---j
----i
-e---j
----i
----j
----i
----j
-
-
-
-*/
