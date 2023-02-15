44b1d413161d77cb9e55aec10b81b9e4cca4b9cd
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-15 11:45:55 +0300

    moving to consider empty string

--- Competitive coding etc/codewars/Question4/scratchpad.c
+++ Competitive coding etc/codewars/Question4/scratchpad.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int checkingIsogram(char *array);
@@ -17,19 +18,31 @@ int main(){
     char string [50];
     printf("Enter a string: ");
     scanf("%s", string);
-    checkingIsogram(string);
+    //checkingIsogram(string);
 
-    //printf("The string is isogram or not: %d", checkingIsogram(string));
+    printf("The string is isogram or not: %d", checkingIsogram(string));
 
 }
 
 int checkingIsogram(char *array){
+    int count =0;
+
     for (int i = 0; array[i] != '\0'; ++i) {
-        printf("The characters in the outer string are %c\n",array[i] ); //ok, finally correctly looping on the array
         for (int j = 0; array[j] != '\0'; ++j){
-            printf("The characters in the inner loop are %c\n",array[j] );
+            if(i!=j){
+                if(tolower(array[i])== tolower(array[j])){
+                    count++;
+                }
+                else
+                    continue;
+
+            }
+
+            //printf("The characters in the inner loop are %c\n",array[j] );
         }
+
     }
+    return count;
 }
 
 /*
