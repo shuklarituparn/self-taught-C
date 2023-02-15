88b1163379a1a3185323b281fe8d44d17aa65c91
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-15 10:55:02 +0300

    added the problem facing now

--- Competitive coding etc/codewars/Question4/scratchpad.c
+++ Competitive coding etc/codewars/Question4/scratchpad.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 int checkingIsogram(char *array);
@@ -24,6 +25,15 @@ int main(){
 
 int checkingIsogram(char *array){
     for (int i = 0; array[i] != '\0'; ++i) {
-        printf("The characters in the string are %c\n",array[i] ); //ok, finally correctly looping on the array
+        printf("The characters in the outer string are %c\n",array[i] ); //ok, finally correctly looping on the array
+        for (int j = 0; array[j] != '\0'; ++j){
+            printf("The characters in the inner loop are %c\n",array[j] );
+        }
     }
 }
+
+/*
+ * The problem in this loop is that it looks at the element in the array and then counts the duplicate element too
+ *
+ *for example: for a string with elements "abc" it counts a while the outer loop is at a or b when outer loop is at b
+ */
