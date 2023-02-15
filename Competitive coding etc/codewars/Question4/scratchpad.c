261707e33a36bf72cb557e138609450c56273b1e
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-15 11:59:11 +0300

    I think I solved it (hopefully)

--- Competitive coding etc/codewars/Question4/scratchpad.c
+++ Competitive coding etc/codewars/Question4/scratchpad.c
@@ -16,7 +16,8 @@ int main(){
 
     char string [50];
     printf("Enter a string: ");
-    scanf("%s", string);
+    fgets(string, 50, stdin);
+    //scanf("%s", string);
     //checkingIsogram(string);
 
     printf("The string is isogram or not: %d", checkingIsogram(string));
@@ -24,13 +25,16 @@ int main(){
 }
 
 int checkingIsogram(char *array){
-    bool flag= false;
+    int count=0;
 
     for (int i = 0; array[i] != '\0'; ++i) {
         for (int j = 0; array[j] != '\0'; ++j){
             if(i!=j){
-                if(tolower(array[i])!= tolower(array[j])){
-                    flag=true;
+                if(tolower(array[i])== tolower(array[j])){
+                    count++;
+                }
+                else if (array[0]==' '){
+                    count=0;
                 }
                 else
                     continue;
@@ -41,10 +45,10 @@ int checkingIsogram(char *array){
         }
 
     }
-    return flag;
+    return count;
 }
 
 /*
-I don't know why I can't solve it using a bool flag. It works good with the count. But everytime I try to declare
- a flag I get error in final solution
+final log: Finally using the fgets() function. I am able to take the space as input and do it with count.
+           With the scanf() it was not counting the spaces, which is a necessary condition for testing in codewars
  */
