e492809a263dff31f7d56fe86d837c6005384834
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-16 17:06:50 +0300

    sorted strings using bubble sort

--- sorting_strings.c
+++ sorting_strings.c
@@ -4,33 +4,43 @@
 #include <stdio.h>
 #include <string.h>
 
-
+void printStrings(char *array);
 int main(){
-    //char temp[30]={0};
+    char temp[30]={0};
     //int result=0;
     char string1[20]={0};
     char string2[20]={0};
-    //char string3[20]={0};
+    char string3[20]={0};
     printf("Enter the first string: ");
     scanf("%s",string1);
     printf("\nEnter the second string: ");
     scanf("%s", string2);
-    //printf("\n Enter the third string: ");
-    //scanf("%s",string3);
-   if(strcmp(string1, string2)<0){
-       printf("String 1 is lexically before of string 2");
-   }
-    if(strcmp(string1, string2)>0){
-        printf("String 1 is lexically after of string 2");
-    }
-    if(strcmp(string1, string2)==0){
-        printf("String 1 is lexically same as string 2");
-    }
+    printf("\n Enter the third string: ");
+    scanf("%s",string3);
 
+    if(strcmp(string1, string2)>0){
+        strcpy(temp, string1);
+        strcpy(string1,string2);
+        strcpy(string2, temp);
+    }
+    else if(strcmp(string2, string3)>0){
+        strcpy(temp, string2);
+        strcpy(string2,string3);
+        strcpy(string3, temp);
+    }
+    printStrings(string1);
+    printStrings(string2);
+    printStrings(string3);
 
     return 0;
+}
 
+void printStrings(char *array){
+    int i=0;
+    while(array[i]!='\0'){
+        printf("%c",array[i]);
 
-
-
+    ++i;
+    }
+    printf("\n");
 }
