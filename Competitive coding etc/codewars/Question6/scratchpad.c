04f49d9903b7205380f224654765727af66ed223
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-19 22:19:57 +0300

    it prints, is it a solution

--- Competitive coding etc/codewars/Question6/scratchpad.c
+++ Competitive coding etc/codewars/Question6/scratchpad.c
@@ -7,16 +7,25 @@
 void print(char *string);
 
 int main(){
-    char str[] = "RqaEzty";
+    char str[] = "aBcDe";
     print(str);
 }
 
 void print( char *string){
     for (int i = 0; string[i]!='\0' ; ++i) {
         for (int j = 0; j <=i; ++j) {
-            printf("%c", string[i]);
+            if(j==0 && i!=0){
+                printf("-");
+                printf("%c", toupper(string[i]));
+            }
+            else if(j==0){
+                //printf("-");
+                printf("%c", toupper(string[i]));
+            }
+            else
+                printf("%c", tolower(string[i]));
+
         }
-        printf("-");
     }
 
 }
