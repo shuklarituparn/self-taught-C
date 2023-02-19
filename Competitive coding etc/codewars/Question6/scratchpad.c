9909784fd8b4b7e676743b40586987b5a5ad8a16
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-19 13:37:27 +0300

    almost corrected the formatting

--- Competitive coding etc/codewars/Question6/scratchpad.c
+++ Competitive coding etc/codewars/Question6/scratchpad.c
@@ -14,13 +14,9 @@ int main(){
 void print( char *string){
     for (int i = 0; string[i]!='\0' ; ++i) {
         for (int j = 0; j <=i; ++j) {
-
-            //printf("%c", string[i]);
-            printf("The value of I: %d\n", i);
-            printf("The value of J: %d\n", j);
-
-
+            printf("%c", string[i]);
         }
+        printf("-");
     }
 
 }
