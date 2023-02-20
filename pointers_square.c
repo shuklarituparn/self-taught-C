3507ea9059a9163b6244045501c7c51ffe7c85c2
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-20 14:45:03 +0300

    more detail in pointers_square.c

--- pointers_square.c
+++ pointers_square.c
@@ -10,6 +10,7 @@ void square(int * x);
 int main()
 {
     int x;
+    printf("The address of the variable x is %p: \n", &x);
     printf("Enter the number you want the square of: ");
     scanf("%d", &x);
 
@@ -19,7 +20,18 @@ int main()
     return 0;
 }
 
-void square(int * x)
+void square(int * x) //pointer that stores the address of the variable
 {
-    *x = (*x) * (*x);
+    printf("The *x is %p\n", x);
+
+    *x = (*x) * (*x); //de-referencing the value stored at the location and using it to calculate square
 }
+
+/*
+ * Here after initialization we get a location or an address of x and then we ask the user for the number at the
+ * location or the address that we already declared.
+ *
+ * Then passing the address of the variable to the second function. After which we de-reference it and use to give
+ * square
+ *
+ */
