11431962c1046a3348d9491c63deff400cbb3382
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-21 11:07:45 +0300

    added challenge_pointer_arithematic

--- challenge_pointer_arithematic.c
+++ challenge_pointer_arithematic.c
@@ -11,9 +11,9 @@ int main(){
     printf("The number of characters in the string are: %d", count_char(string));
 }
 int count_char(const char *string){
-    const char *pChar= string;
-    while(*string){
-        *string++;
+    const char *pChar= string;  //storing the first address of the array
+    while(*string){    //checking if the dereferenced string is the null terminator
+        string++;   //incrementing the string
     }
-    return string-pChar;
+    return string-pChar;    //subtracting the end address from the beginning to give the size of the string
 }
