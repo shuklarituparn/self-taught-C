9ac9e4de2fec2f6aeb88039a37ff92af5599a1d2
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-23 12:01:21 +0300

    added opening_closing_files

---
+++ opening_closing_files.c
@@ -0,0 +1,38 @@
+//
+// Created by rituparn on 23.02.23.
+//
+
+/*
+ * Create a file, and open a file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    FILE *fPtr;
+
+    char str[20];
+
+    if(!fPtr){
+        printf("Error in opening a file: ");
+        return 1;
+    }
+    fPtr=fopen("New_file.txt","w+");    //Can observe a new file created
+    printf("Enter the data for the file named ");
+
+    fgets(str, 20, stdin);
+    fputs(str, fPtr);
+    fclose(fPtr);
+
+    return 0;
+}
+
+/*
+ * Can actually write to the file but it has a limit which is hard-coded in to the program and need to be hard-coded
+ *
+ * Will try with malloc(), and calloc()
+ */
+
+//As we can see that it deletes the data that was in the file before and writes a new one in it's place
