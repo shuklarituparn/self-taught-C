f1625fc9f597f10112c1c0237991a0f2f2a7e100
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-03 09:49:07 +0300

    added file

--- fputs.c
+++ fputs.c
@@ -1,3 +1,20 @@
 //
 // Created by rituparn on 23.02.23.
 //
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main (){
+
+    FILE *fPtr;
+    char str[80];
+    if((fPtr= fopen("New_file.txt","w"))==NULL){
+        printf("Error in opening file: \n");
+        return 1;
+    }
+    printf("Enter the text\n");
+    printf("To stop entering, press Ctrl+d/Ctrl+z\n");
+
+}
