d438f8b253c5af98686f9c8f38e23c1ad67dc854
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-03 12:22:34 +0300

    added code for linked_list in C

---
+++ Algorithms/Linked_list/reverse.c
@@ -0,0 +1,22 @@
+//
+// Created by rituparn on 03.04.23.
+//
+#include<stdio.h>
+#include<stdlib.h>   //for the malloc
+
+struct Node
+{
+    char data;
+    struct Node *next;
+}*head=NULL;
+
+
+
+
+int main(){
+
+    create(5);
+
+
+
+}
