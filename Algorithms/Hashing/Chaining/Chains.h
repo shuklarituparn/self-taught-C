c232085dc2667f220af41919b46bb08f2a5f28d9
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-05-01 17:20:07 +0300

    added hashing in C

---
+++ Algorithms/Hashing/Chaining/Chains.h
@@ -0,0 +1,48 @@
+//
+// Created by rituparn on 01.05.23.
+//
+#ifndef CHAINS_H
+#define CHAINS_H
+
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void SortedInsert(struct Node **H, int x) {
+    struct Node *t, *q= NULL, *p=*H;
+    t=(struct Node*) malloc(sizeof(struct Node));
+    t->data=x;
+    t->next=NULL;
+
+    if(*H==NULL)
+        *H=t;
+    else{
+        while (p&&p->data<x){
+            q=p;
+            p=p->next;
+        }
+        if(p==*H){
+            t->next=*H;
+            *H=t;
+        }
+        else{
+            t->next=q->next;
+            q->next=t;
+        }
+    }
+}
+
+struct Node *Search(struct Node *p, int key) {
+    while(p!=NULL){
+        if(key==p->data){
+            return p;
+        }
+        p=p->next;
+    }
+    return NULL;
+}
+
+#endif // CHAINS_H
